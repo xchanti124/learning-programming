2e18da80a7aa010a45963c16e677061b059252e8
Chantal Ariu <chantalariu227@gmail.com>
2022-10-17 21:28:49 +0200

    Complete searchInsertPosition exercise

---
+++ leet-code-exercises/searchInsertPosition.ts
@@ -0,0 +1,11 @@
+const searchInsert = (nums: number[], target: number): number => {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] >= target) {
+      return i;
+    }
+  }
+  return nums.length;
+};
+
+console.log(searchInsert([1, 3, 5, 6], 5));
+console.log(searchInsert([1, 3, 5, 6], 2));
