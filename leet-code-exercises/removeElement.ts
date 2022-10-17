6ac406f1ff709bf2aa82cc01623f51d2aaa510b2
Chantal Ariu <chantalariu227@gmail.com>
2022-10-17 21:28:12 +0200

    Complete removeElement exercise

---
+++ leet-code-exercises/removeElement.ts
@@ -0,0 +1,10 @@
+function removeElement(nums: number[], val: number): number {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === val) {
+      nums.splice(i, 1);
+      i--;
+    }
+  }
+  return nums.length;
+}
+console.log(removeElement([1, 1, 3, 4, 5], 1));
