063f4928b80ddcb99b84e6f961180af65a74ef87
Chantal Ariu <chantalariu227@gmail.com>
2023-03-23 18:57:33 +0100

    Complete binarySearch exercise

--- leet-code-exercises/binarySearch.ts
+++ leet-code-exercises/binarySearch.ts
@@ -1,19 +1,26 @@
-function search(
+const search = (
   nums: number[],
   target: number,
   l = 0,
   r = nums.length - 1
-): number {
-  if (l > r) {
+): number => {
+  if (r < l) {
     return -1;
   }
+
   const middleIndex = ((l + r) / 2) | 0;
+  // Bitwise or (| 0) will just return the actual number, but as an integer. It's the same as doing Math.floor()
   const middleNumber = nums[middleIndex];
+
   if (middleNumber === target) {
     return middleIndex;
   }
-  if (target < middleNumber) {
-    return search(nums, target, l, middleIndex - 1);
+
+  if (target > middleNumber) {
+    return search(nums, target, middleIndex + 1, r);
   }
-  return search(nums, target, middleIndex + 1, r);
-}
+
+  return search(nums, target, l, middleIndex - 1);
+};
+
+console.log(search([-1, 0, 3, 5, 9, 12], 9));
